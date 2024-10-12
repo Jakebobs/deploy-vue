42ce96e2173ddf6b70e517a52d2df3f2d4631540
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 15:07:45 +0200

    change shell

--- VuePagesTutorial/foreground.sh
+++ VuePagesTutorial/foreground.sh
@@ -2,10 +2,7 @@
 sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo snap install gh
+
 
 echo "Installation complete!"
-
-sudo snap install gh
-gh auth login
-
-
