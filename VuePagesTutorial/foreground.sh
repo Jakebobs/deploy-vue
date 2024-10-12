dbfa45e5d7f9b5da5a82a6e453312ae0b0a5f8c1
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 15:01:24 +0200

    new shell

--- VuePagesTutorial/foreground.sh
+++ VuePagesTutorial/foreground.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-# This PAT only has access to the repository that contains the tutorial content
-sudo snap install gh
-gh auth login
-
 sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo "Installation complete!"
+
+sudo snap install gh
+gh auth login
+
+
