7364b2d01bad02918c3bd63bff4913f26309b44e
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 14:14:51 +0200

    git credentials

--- VuePagesTutorial/foreground.sh
+++ VuePagesTutorial/foreground.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config user.email "Tutorial@example.com"
+git config user.name "Tutorial Taker"
+
 sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
