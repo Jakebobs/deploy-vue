44ec348a271186acf6805d88d34c208d9027a8e1
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 13:48:11 +0200

    install v3

--- VuePagesTutorial/foreground.sh
+++ VuePagesTutorial/foreground.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Step 1: Installation of npm
-#clear
-#echo "Installing npm..."
-#sudo apt-get install npm -y
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
 
-# Step 2: Installation of nodejs
-#clear
-#echo "Installing nodejs..."
-#sudo apt-get install nodejs -y
 
 # Step 3: Display that the installation is complete
 echo "Installation complete!"
