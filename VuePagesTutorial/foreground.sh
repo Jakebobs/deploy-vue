9dda25300e96f886f72a8b842a833054574159d6
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 13:50:13 +0200

    official node installer

--- VuePagesTutorial/foreground.sh
+++ VuePagesTutorial/foreground.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
+sudo apt-get update
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-
-# Step 3: Display that the installation is complete
 echo "Installation complete!"
