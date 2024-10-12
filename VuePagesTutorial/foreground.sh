fdf4356e946240ff97b33fe9b5051cc995e7605f
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 14:58:42 +0200

    new auth method

--- VuePagesTutorial/foreground.sh
+++ VuePagesTutorial/foreground.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 # This PAT only has access to the repository that contains the tutorial content
-git clone https://miljon3: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@github.com/miljon3/porkchop.io.git
-
-# Configure git user details
-git config --global user.email "Tutorial@example.com"
-git config --global user.name "Tutorial Taker"
-
-# Remove all files from the cloned repository
-cd porkchop.io
-rm -rf *
-
-cd ..
+sudo snap install gh
+gh auth login
 
 sudo apt-get update
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
